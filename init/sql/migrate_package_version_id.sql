cfb8dd3552be6c40f5d6c723058e583e7ba49b8a
Mike Hoefer <mhoefer@salesforce.com>
2018-08-20 19:48:46 -0700

    Cleanup

--- init/sql/migrate_package_version_id.sql
+++ init/sql/migrate_package_version_id.sql
@@ -1,3 +1,5 @@
+-- Phase 1 - Migrate Em
+
 -- org package version
 alter table org_package_version
   add if not exists version_id varchar(18);
@@ -7,8 +9,6 @@ set version_id = v.version_id
 from package_version v
 where a.package_version_id = v.sfid;
 
-alter table org_package_version
-  drop column if exists package_version_id;
 
 -- upgrade item
 alter table upgrade_item
@@ -16,9 +16,6 @@ alter table upgrade_item
 
 update upgrade_item 
 set version_id = package_version_id;
-
-alter table upgrade_item
-  drop column if exists package_version_id;
 
 -- upgrade job
 update upgrade_job a
@@ -35,9 +32,7 @@ set version_id = v.version_id
 from package_version v
 where a.package_version_id = v.sfid;
 
-alter table license
-  drop column if exists package_version_id;
-
+-- Indices
 drop index if exists license_org_version_index;
 create index if not exists license_org_version_index
   on license (org_id, version_id);
@@ -45,3 +40,15 @@ create index if not exists license_org_version_index
 drop index if exists license_package_org_version_index;
 create index if not exists license_package_org_version_index
   on license (org_id, package_id, version_id);
+
+
+-- Phase 2 - Nuke Em
+
+alter table org_package_version
+  drop column if exists package_version_id;
+
+alter table upgrade_item
+  drop column if exists package_version_id;
+
+alter table license
+  drop column if exists package_version_id;
