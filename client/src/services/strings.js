0ed151a5fe4568f16f081edcf99677d50f728f13
Mike Hoefer <mhoefer@salesforce.com>
2019-01-29 11:23:00 -0800

    Support passing the length explicitly, not just by passing a collection

--- client/src/services/strings.js
+++ client/src/services/strings.js
@@ -2,7 +2,7 @@ const englishNums = [
 	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]; 
 
 export let pluralizeIt = (arr, str, pluralSuffix = "s") => {
-	const len = arr.size ? arr.size : arr.length;
+	const len = arr.size ? arr.size : arr.length || arr;
 	const num = len < englishNums.length ? englishNums[len] : generalizeIt(len);
 	const suffix = len === 0 || len > 1 ? pluralSuffix : "";
 	return {num, str: (str + suffix)}
