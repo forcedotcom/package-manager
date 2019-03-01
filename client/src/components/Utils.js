7e18ca6ed8f790442ab42dfe2b96181f763f5e13
Mike Hoefer <mhoefer@salesforce.com>
2019-03-01 15:22:17 -0800

    Better version upgrade arrow icon.  Don't confuse with <= or => operators

--- client/src/components/Utils.js
+++ client/src/components/Utils.js
@@ -11,5 +11,5 @@ export const renderVersionNumber = row => {
 		return row.version_number;
 
 	return <span title="An upgrade to a newer version is available for this org" style={{borderRadius: "4px", margin: 0, fontWeight: "bold", padding: "2px 4px 2px 4px"}}
-				 className="slds-theme--success">{row.version_number} =&gt; {row.latest_version_number}</span>;
+				 className="slds-theme--success">{row.version_number} {'\u279a'} {row.latest_version_number}</span>;
 };
