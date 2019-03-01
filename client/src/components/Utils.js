a2671da1c77461fe777b1d82d1f4aa2658b0dbd5
Mike Hoefer <mhoefer@salesforce.com>
2019-03-01 15:25:37 -0800

    Better tooltip messaging to explicitly state available upgrade version

--- client/src/components/Utils.js
+++ client/src/components/Utils.js
@@ -10,6 +10,6 @@ export const renderVersionNumber = row => {
 	if (row.version_sort >= row.latest_version_sort)
 		return row.version_number;
 
-	return <span title="An upgrade to a newer version is available for this org" style={{borderRadius: "4px", margin: 0, fontWeight: "bold", padding: "2px 4px 2px 4px"}}
+	return <span title={`An upgrade to ${row.package_name} ${row.latest_version_number} is available for this org`} style={{borderRadius: "4px", margin: 0, fontWeight: "bold", padding: "2px 4px 2px 4px"}}
 				 className="slds-theme--success">{row.version_number} {'\u279a'} {row.latest_version_number}</span>;
 };
