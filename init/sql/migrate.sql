6df484df58c5ca9197ae75d674428d216674aac8
Anand Bikkavilli <abikkavilli@salesforce.com>
2019-11-18 14:51:31 -0800

    fix syntax

--- init/sql/migrate.sql
+++ init/sql/migrate.sql
@@ -1,3 +1,4 @@
 -- Add any data model changes, data fixes or new indices here.  Can be removed once all deployments are upgraded.
 
-alter table update add column comment text;
+alter table upgrade
+  add if not exists comment text;
