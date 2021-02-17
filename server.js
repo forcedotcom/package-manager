e8042a36614bd563da1a28f60139d9af5f6a7262
Mike Hoefer <mhoefer@salesforce.com>
2021-02-16 16:22:14 -0800

    Try a CSP allowing inline scripting only

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const app = express();
 app.use(helmet({
 	contentSecurityPolicy: {
 		directives: {
-			scriptSrc: ["'self'", "'unsafe-inline'"]
+			defaultSrc: ["'self'", "'unsafe-inline'"]
 		}
 	}
 }));
